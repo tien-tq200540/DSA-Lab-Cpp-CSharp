64ca2051415a8f2005ce199228528f67463dbc72
tien-tq200540 <tqtien157202@gmail.com>
2025-08-26 13:56:59 +0700

    [Cpp][Array] Refactor: Split into 2 distinct file for implement and demo instead of 1 file

--- Cpp/Array/dynamic_array.cpp
+++ Cpp/Array/dynamic_array.cpp
@@ -101,27 +101,3 @@ void DynamicArray::rotate(int step) {
 				step--;
 			}
 }
-
-int main() {
-	//DEMO
-	DynamicArray myArray;
-	for (int i = 0; i < 10; i++) myArray.push_back(i);
-	cout << "My array: "; myArray.print(); cout << endl;
-	cout << "Size of array = " << myArray.size() << endl;
-	cout << "Min = " << myArray.get_min() << " and max = " << myArray.get_max() << endl;
-	
-	cout << "--- Reverse ---\n"; 
-	myArray.reverse();
-	cout << "After reverse, my array: "; myArray.print(); cout << endl;
-	myArray.reverse();
-	cout << "Reverse again to get old array: "; myArray.print(); cout << endl;
-	
-	try {
-		cout << "--- Rotate ---\n";
-		myArray.rotate(5);
-		cout << "After rotate, my array: "; myArray.print(); cout << endl;
-	} catch (exception& e) {
-		cout << "Caught: " << e.what() << endl;
-	}
-	return 0;
-}
